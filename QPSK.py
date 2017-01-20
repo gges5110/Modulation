f7ce702eb9293ba4e85c7de6dbdd9a37d80df0f0
Gerry Wu <gerry.wu@ni.com>
2017-01-20 12:51:49 -0600

    QPSK

--- QPSK.py
+++ QPSK.py
@@ -3,6 +3,36 @@ from random import random
 from matplotlib import pyplot as plt
 import numpy as np
 
+def symbol_modulate(QPSK_symbol):
+    I_sign = 0
+    Q_sign = 0
+
+    if QPSK_symbol[0] == 0:
+        I_sign = -1
+    else:
+        I_sign = 1
+
+    if QPSK_symbol[1] == 0:
+        Q_sign = -1
+    else:
+        Q_sign = 1
+
+    return I_sign, Q_sign
+
+def symbol_demodulate(QPSK_signal):
+    I_data = 0
+    Q_data = 0
+    if QPSK_signal[0] > 0:
+        I_data = 1
+    else:
+        I_data = 0
+
+    if QPSK_signal[1] > 0:
+        Q_data = 1
+    else:
+        Q_data = 0
+
+    return I_data, Q_data
 
 Fs = 8000
 carrier_frequency = 200
@@ -11,9 +41,10 @@ samples = Fs / carrier_frequency * data_size / 2
 samples_per_symbol = Fs / carrier_frequency
 
 
-data = np.arange(data_size)
+
 
 # Data generation
+data = np.arange(data_size)
 for n in range(0, data_size):
     if random() > 0.5:
         data[n] = 1
@@ -47,22 +78,15 @@ plt.draw()
 
 # Modulate carrier with I/Q data
 I_modulated = np.arange(samples, dtype=np.float_)
-for n in range(len(I_data)):
-    sign = 1 if I_data[n] == 1 else -1
-
-    start = n * samples_per_symbol
-    end = (n + 1) * samples_per_symbol
-
-    I_modulated[start: end] = sign * I_carrier[start: end]
-
 Q_modulated = np.arange(samples, dtype=np.float_)
-for n in range(len(Q_data)):
-    sign = 1 if Q_data[n] == 1 else -1
 
+for n in range(data_size / 2):
+    I_sign, Q_sign = symbol_modulate(data[n * 2:n * 2 + 2])
     start = n * samples_per_symbol
     end = (n + 1) * samples_per_symbol
 
-    Q_modulated[start: end] = Q_carrier[start: end] * sign
+    I_modulated[start: end] = I_sign * I_carrier[start: end]
+    Q_modulated[start: end] = Q_sign * Q_carrier[start: end]
 
 
 # Add the I/Q data in time domain
@@ -76,26 +100,20 @@ plt.draw()
 receive_signal = transmit_signal
 
 # Separate I/Q data and demodulate
-
 I_demodulated = np.arange(len(I_data))
-for n in range(len(I_data)):
-    start = n * samples_per_symbol
-    end = (n + 1) * samples_per_symbol
-
-    temp = np.sum(I_modulated[start: end] * I_carrier[start: end])
-    I_demodulated[n] = 0 if temp < 0 else 1
-
-
 Q_demodulated = np.arange(len(Q_data))
+
 for n in range(len(I_data)):
     start = n * samples_per_symbol
     end = (n + 1) * samples_per_symbol
 
-    temp = np.sum(Q_modulated[start: end] * Q_carrier[start: end])
-    Q_demodulated[n] = 0 if temp < 0 else 1
+    I_integral = np.sum(receive_signal[start: end] * I_carrier[start: end])
+    Q_integral = np.sum(receive_signal[start: end] * Q_carrier[start: end])
+
+    I_demodulated[n], Q_demodulated[n] = symbol_demodulate([I_integral, Q_integral])
+
 
 print "I demodulated:", I_demodulated, "actual data:", I_data
 print "Q demodulated:", Q_demodulated, "actual data:", Q_data
 
 plt.show()
-
